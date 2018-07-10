91768e52dc1f25578d7bd3bc430aeff06293be77
Kok Ben Toh <kokbent@ufl.edu>
2018-07-10 16:19:32 -0400

    incorporate new changes from Denis

--- tool functions.R
+++ tool functions.R
@@ -1,15 +1,5 @@
-convert.matrix=function(grid1,var1,uni.x,uni.y){
-  nx=length(uni.x)
-  grid2=matrix(NA,nx,nx)
-  for (i in 1:nx){
-    cond=grid1$x==uni.x[i]
-    tmp=grid1[cond,]
-    tmp1=tmp[order(tmp$y),]
-    grid2[,i]=tmp1[,var1]
-  }
-  round(grid2,3)
-}
-get.incid.preval=function(coef1,grid1,x.hf.new,y.hf.new){
+get.incid.preval=function(coef2,grid1,x.hf.new,y.hf.new,stats){
+  #get distance to nearest health facility
   nx=length(x.hf.new)
   dist.mat=matrix(NA,nrow(grid1),nx)
   for (i in 1:nx){
@@ -17,10 +7,15 @@ get.incid.preval=function(coef1,grid1,x.hf.new,y.hf.new){
     y1=(grid1$y-y.hf.new[i])^2
     dist.mat[,i]=sqrt(x1+y1)
   }
-  grid1$dist1=apply(cbind(grid1$dist.orig,dist.mat),1,min)
+  grid1$dist1=apply(cbind(grid1$dist_hf,dist.mat),1,min)
+  grid1$dist_hf.sd=(grid1$dist1-stats['media','dist_hf'])/stats['sd1','dist_hf']
   
-  tmp=coef1[1]+coef1[,'Dist_HF']*grid1$dist1+coef1[,'wealth']*grid1$wealth+
-    coef1[,'Dist_HF.wealth']*grid1$dist1*grid1$wealth
+  #calculate malaria prevalence and population at risk
+  tmp=coef2['(Intercept)']+
+    coef2['dist_hf']*grid1$dist_hf.sd+
+    coef2['dist_UC']*grid1$dist_uc.sd+
+    coef2['dist_hf:dist_UC']*grid1$dist_hf.sd*grid1$dist_uc.sd
+  
   grid1$preval=exp(tmp)/(1+exp(tmp))
   grid1$incid=grid1$preval*grid1$pop
   grid1
--- toolHF.R
+++ toolHF.R
@@ -7,14 +7,32 @@ library(ggplot2)
 
 source('tool functions.R')
 
-#get data
-grid1=read.csv('spat/fake data gridded precalc.csv',as.is=T)
-hf=read.csv('spat/fake data hf coord.csv',as.is=T)
-opt1=read.csv('spat/optimized hf coord.csv',as.is=T)
-pop=read.csv('spat/fake data popcenter.csv',as.is=T)
+# grid and location data
+grid1=read.csv('data/fake data gridded precalc.csv',as.is=T)
+hf=read.csv('data/fake data hf coord.csv',as.is=T)
+opt1=read.csv('data/optimized hf coord.csv',as.is=T)
+pop=read.csv('data/fake data popcenter.csv',as.is=T)
 pop$type="PopCen"
-coef1=data.matrix(read.csv('spat/glm table coef.csv',as.is=T))
 
+# model data
+coef1=read.csv('data/glmer coeff.csv',as.is=T)
+coef2=coef1[,'Estimate']
+names(coef2)=coef1$X
+stats=read.csv('data/glmer covariate stats.csv',as.is=T)
+rownames(stats)=stats$X
+grid1$dist_uc.sd=(grid1$dist_uc-stats['media','dist_UC'])/stats['sd1','dist_UC']
+
+# Miscellaneous data
+s.incid.base=sum(grid1$incid)
+size.land=15
+
+# Debug use
+# input=list()
+# input$x='0.5,0.2'
+# input$y='0.5,0.1'
+# input$nhf=2
+# input$tipo='Population at risk'
+# input$opcao='Optimized'
 
 
 # Define UI 
@@ -23,8 +41,7 @@ ui <- dashboardPage(
   dashboardSidebar(
     radioButtons(inputId = "tipo", 
                  label = "Type:", 
-                 choices = c("Population at risk" = "PAR",
-                             "Prevalence" = "Prev")),
+                 choices = c("Population at risk", "Prevalence")),
     radioButtons(inputId = "opcao", 
                  label = "Option:", 
                  choices = c("User-defined", "Optimized")),
@@ -52,16 +69,16 @@ ui <- dashboardPage(
           width = NULL,
           status = "primary",
           solidHeader = T,
-          plotOutput("MalariaPlot", height = "600px")
+          plotOutput("malariaPlot", height = "600px")
         )
       ),
       column(
         width = 4,
-        box(title = "Wealth distribution", width = NULL, plotOutput("wealthPlot"),
+        box(title = "Distance to HF", width = NULL, plotOutput("distPlot"),
+            collapsible = T, solidHeader = T),
+        box(title = "Distance to urban center", width = NULL, plotOutput("distucPlot"),
             collapsible = T, solidHeader = T),
         box(title = "Population density", width = NULL, plotOutput("popPlot"),
-            collapsible = T, solidHeader = T),
-        box(title = "Distance to HF", width = NULL, plotOutput("distPlot"),
             collapsible = T, solidHeader = T)
       )
     )
@@ -70,110 +87,103 @@ ui <- dashboardPage(
 
 # Define server logic 
 server <- function(input, output) {
-  s.incid.base=sum(grid1$incid)
   
-  outPlot <- reactive({
-    var1='preval'; max1=1; nome='Prevalence'
-    if (input$tipo=='Cases') {var1='incid';max1=2210; nome='Cases'}
+  out.plot <- reactive({
+    # Prevalence or incidence
+    nome=input$tipo
+    var1=ifelse(nome == 'Prevalence', 'preval', 'incid')
     
+    # Calculate grid and hf data based on User-defined coordinates
     if (input$opcao=='User-defined'){
+      # Convert coordinates with commas to vector
       tmp=unlist(strsplit(input$x,split=','))
       x.user=as.numeric(tmp); nx=length(x.user)
       tmp=unlist(strsplit(input$y,split=','))
       y.user=as.numeric(tmp); ny=length(y.user)
-      xlab1=0.3
-      ylab1=0.9
       
-      grid2=get.incid.preval(coef1,grid1,x.hf.new=0.25,y.hf.new=0.75)
-      grid2$zzz=grid2[,var1]
+      # Get predicted prevalence and pop at risk
+      grid.user=get.incid.preval(coef2, grid1, x.hf.new=0.25*size.land, 
+                                 y.hf.new=0.75*size.land, stats)
+      grid.user$zzz=grid.user[,var1]
+      # prop.reduction=round(100*sum(grid.user$incid)/s.incid.base,0)
+      hf.user=hf
       
-      
+      # Check if there are new coordinates and equal no. of x and y
+      # Then update incid and prev
       if (nx==ny & nx!=0 & ny!=0){
-        grid2=get.incid.preval(coef1,grid1,x.hf.new=x.user,y.hf.new=y.user)
-        grid2$zzz=grid2[,var1]
-        hf1=data.frame(x=c(hf$x,x.user),y=c(hf$y,y.user))
-        hf1$type=ifelse(hf1$x==hf$x & hf1$y==hf$y, "HFExist", "HFNew")
+        grid.user=get.incid.preval(coef2, grid1, x.hf.new=x.user,
+                                   y.hf.new=y.user, stats)
+        grid.user$zzz=grid.user[,var1]
+        hf.user=data.frame(x=c(hf$x, x.user), y=c(hf$y, y.user))
+        hf.user$type=ifelse(hf.user$x==hf$x & hf.user$y==hf$y, "HFExist", "HFNew")
       }
-      
-      hf1=rbind(hf1,pop)
-      hf1$type=factor(hf1$type, levels = c("HFExist", "HFNew", "PopCen"))
-      propCase=round(100*sum(grid2$incid)/s.incid.base,0)
-      label1=paste("Proportion original cases = ",propCase,'%', sep='')
-      label2=paste(100-propCase, '% of cases reduced', sep='')
-      label=paste(label1, ', ', label2, sep = '')
     }
     
     if (input$opcao=='Optimized'){
       cond=as.numeric(input$nhf)==opt1$nhf
-      grid2=get.incid.preval(coef1,grid1,x.hf.new=opt1$x[cond],y.hf.new=opt1$y[cond])
-      hf1=data.frame(x=c(hf$x,opt1$x[cond]),y=c(hf$y,opt1$y[cond]),
+      grid.user=get.incid.preval(coef2, grid1, x.hf.new=opt1$x[cond],
+                                 y.hf.new=opt1$y[cond], stats)
+      grid.user$zzz=grid.user[,var1]
+      hf.user=data.frame(x=c(hf$x, opt1$x[cond]),y=c(hf$y, opt1$y[cond]),
                      type=c("HFExist", rep("HFNew", input$nhf)))
-      hf1=rbind(hf1,pop)
-      hf1$type=factor(hf1$type, levels = c("HFExist", "HFNew", "PopCen"))
-      grid2$zzz=grid2[,var1]
-      
-      propCase=round(100*sum(grid2$incid)/s.incid.base,0)
-      label1=paste("Proportion original cases = ",propCase,'%', sep='')
-      label2=paste(100-propCase, '% of cases reduced', sep='')
-      label=paste(label1, ', ', label2, sep = '')
-      
-      
     }
+    
+    # Make table of point of interests
+    poi=rbind(hf.user,pop)
+    poi$type=factor(poi$type, levels = c("HFExist", "HFNew", "PopCen"))
+    prop.reduction=round(100*sum(grid.user$incid)/s.incid.base,0)
+    label1=paste("Proportion original cases = ", prop.reduction, '%', sep='')
+    label2=paste(100-prop.reduction, '% of cases reduced', sep='')
+    label=paste(label1, ', ', label2, sep = '')
+    
     # Basic frameworks for output plot
-    res= ggplot() + 
-      xlab('Longitude') + ylab("Latitude") + theme_bw(base_size = 11) +
+    res <- ggplot() + 
+      xlab('X') + ylab("Y") + theme_bw(base_size = 11) +
       coord_fixed()
     
     # Main plot
-    res1=res + 
-      geom_tile(data = grid2, alpha = 0.8,aes(x = x, y = y,fill = zzz)) +
-      scale_fill_gradient2(low = "cyan", mid = "red",high='purple',
-                           limits=c(0,max1),midpoint=max1/2,
-                           name = nome) + 
-      geom_point(data = hf1, aes(x = x,y=y,shape=type),size=5) +
+    max1 <- ceiling(max(grid1[,var1]))
+    plot.main=res + 
+      geom_tile(data=grid.user, alpha=0.8, aes(x=x, y=y, fill=zzz)) +
+      scale_fill_gradient2(low="cyan", mid="red", high='purple',
+                           limits=c(0,max1), midpoint=max1/2, name=nome) + 
+      geom_point(data=poi, aes(x=x, y=y, shape=type), size=5) +
       scale_shape_manual(name="Point of Interest", 
                          breaks=c("HFExist", "HFNew", "PopCen"),
                          labels=c("Existing HF", "Proposed HF", "Population Center"),
                          values=c(17, 2, 10), drop=F) +
       ggtitle(label)
     
-    # Wealth plot
-    wealthPlot=res + 
-      geom_tile(data = grid2, alpha = 0.8,aes(x = x, y = y,fill = wealth)) +
-      scale_fill_gradient(low = "#ffffff", high = "#ff0000", limits = c(0, 1), name = '') + 
-      geom_point(data = hf1, aes(x = x, y=y, shape=type),size=5,show.legend = F) +
+    # Sub plots template
+    res1=res + 
+      scale_fill_gradient(low='#ffffff', high='#ff0000', name='') +
       scale_shape_manual(name="Point of Interest", 
                          breaks=c("HFExist", "HFNew", "PopCen"),
                          labels=c("Existing HF", "Proposed HF", "Population Center"),
                          values=c(17, 2, 10), drop=F)
+    # Wealth plot
+    plot.distuc=res1 + 
+      geom_tile(data=grid.user, alpha=0.8, aes(x=x, y=y, fill=dist_uc)) +
+      geom_point(data=poi, aes(x=x, y=y, shape=type), size=4, show.legend=F)
     
     # Pop plot
-    popPlot=res + 
-      geom_tile(data = grid2, alpha = 0.8,aes(x = x, y = y,fill = pop)) +
-      scale_fill_gradient(low = "#ffffff", high = "#ff0000", name = '') + 
-      geom_point(data = hf1, aes(x = x,y=y,shape=type),size=5, show.legend = F) +
-      scale_shape_manual(name="Point of Interest", 
-                         breaks=c("HFExist", "HFNew", "PopCen"),
-                         labels=c("Existing HF", "Proposed HF", "Population Center"),
-                         values=c(17, 2, 10), drop=F)
+    plot.pop=res1 + 
+      geom_tile(data=grid.user, alpha=0.8, aes(x=x, y=y, fill=pop)) +
+      geom_point(data=poi, aes(x=x, y=y, shape=type), size=4, show.legend=F)
     
     # Dist plot
-    distPlot=res + 
-      geom_tile(data = grid2, alpha = 0.8,aes(x = x, y = y,fill = dist1)) +
-      scale_fill_gradient(low = "#ffffff", high = "#ff0000", name = '') + 
-      geom_point(data = hf1, aes(x = x,y=y,shape=type),size=5, show.legend = F) +
-      scale_shape_manual(name="Point of Interest", 
-                         breaks=c("HFExist", "HFNew", "PopCen"),
-                         labels=c("Existing HF", "Proposed HF", "Population Center"),
-                         values=c(17, 2, 10), drop=F)
+    plot.dist=res1 + 
+      geom_tile(data=grid.user, alpha=0.8, aes(x=x, y=y, fill=dist1)) +
+      geom_point(data=poi, aes(x=x, y=y, shape=type), size=4, show.legend=F)
     
-    L <- list(res1, wealthPlot, popPlot, distPlot)
+    L <- list(plot.main=plot.main, plot.distuc=plot.distuc, 
+              plot.pop=plot.pop, plot.dist=plot.dist)
   })
   
-  output$MalariaPlot <- renderPlot(outPlot()[[1]])
-  output$wealthPlot <- renderPlot(outPlot()[[2]])
-  output$popPlot <- renderPlot(outPlot()[[3]])
-  output$distPlot <- renderPlot(outPlot()[[4]])
+  output$malariaPlot <- renderPlot(out.plot()$plot.main)
+  output$distucPlot <- renderPlot(out.plot()$plot.distuc)
+  output$popPlot <- renderPlot(out.plot()$plot.pop)
+  output$distPlot <- renderPlot(out.plot()$plot.dist)
 }
 
 # Run the application 
